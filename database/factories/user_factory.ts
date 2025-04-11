b7867bd4f71d57bbbe7cbc1b331576aa667c90a3
Emmanuel Mayele <emmamayele123@gmail.com>
2025-04-11 19:56:43 +0100

    rectification des dossiers

---
+++ database/factories/user_factory.ts
@@ -0,0 +1,15 @@
+import  User  from '#models/user';
+import factory from '@adonisjs/lucid/factories'
+
+export const UserFactory = factory
+  .define(User, async ({ faker }) => {
+    const firstName = faker.person.firstName().charAt(0).toUpperCase()
+    const lastName = faker.person.lastName().charAt(0).toUpperCase()
+    return {
+      firstName: firstName,
+      lastName: lastName,
+      email: faker.internet.email(),
+      password: 'password1234',
+    }
+  })
+  .build()
